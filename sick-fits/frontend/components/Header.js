ee8fedeb7ec047ef99946fcb43e09437481d212a
novokaine <sergiu.ojoc@gmail.com>
2021-07-01 08:43:18 +0300

    End of lesson 7

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -20,7 +20,7 @@ const Logo = styled.h1`
 
 const HeaderStyles = styled.header`
   .bar {
-    border-bottom: 10px solid var(--black, black);
+    border-bottom: 10px solid var(--black);
     display: grid;
     grid-template-columns: auto 1fr;
     justify-content: space-between;
@@ -30,7 +30,7 @@ const HeaderStyles = styled.header`
   .sub-bar {
     display: grid;
     grid-template-columns: 1fr auto;
-    border-bottom: 1px solid var(--black, black);
+    border-bottom: 1px solid var(--black);
   }
 `;
 
@@ -38,7 +38,7 @@ const Header = () => (
   <HeaderStyles>
     <div className="bar">
       <Logo>
-        <Link href="/">Home</Link>
+        <Link href="/">Sick fits</Link>
       </Logo>
     </div>
     <div className="sub-bar">
--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,11 +1,65 @@
 import Proptypes from 'prop-types';
+import styled, { createGlobalStyle } from 'styled-components';
 import Header from './Header';
+
+const GlobalStyles = createGlobalStyle`
+  @font-face{
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2')
+    format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  html {
+    --red: #ff0000;
+    --black: #393939;
+    --grey: #3a3a3a;
+    --gray: var(--grey);
+    --lightGrey: #e1e1e1;
+    --lightGray: var(--lightGrey);
+    --offwhite: #ededed;
+    --maxWidth: 1000px;
+    --box-shadow: 0 12px 24px 0 rgba(0,0,0,0.9);
+    box-sizing: border-box;
+
+  }
+
+  *, *:before, *:after{
+    box-sizing: inherit;
+  }
+
+  body{
+    font-family: 'radnika_next', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    margin: 0;
+    padding: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+  }
+
+  a {
+    text-decoration: none;
+    color: var(--black);
+    &:hover{
+      text-decoration: underline;
+    }
+  }
+
+  button{
+    font-family: 'radnika_next'
+  }
+`;
+
+const InnerStyles = styled.div`
+  max-width: var(--maxWidth);
+  padding: 2rem;
+`;
 
 const Page = ({ children }) => (
   <div>
+    <GlobalStyles />
     <Header />
-    <p>Here is the page component</p>
-    {children}
+    <InnerStyles>{children}</InnerStyles>
   </div>
 );
 
