20349ca295cf819b4904a036a5c3c9ec689fe0f9
Sergiu <sergiu@Sergius-MacBook-Pro.local>
2021-07-01 06:40:42 +0300

    Lesson 6 ready

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,14 +1,50 @@
 import Link from 'next/link';
+import styled from 'styled-components';
+import Nav from './Nav';
+
+const Logo = styled.h1`
+  background: red;
+  font-size: 4rem;
+  margin: 4rem;
+  position: relative;
+  z-index: 2;
+  transform: skew(-7deg);
+
+  a {
+    color: #fff;
+    text-decoration: none;
+    padding: 0.5rem;
+    text-transform: uppercase;
+  }
+`;
+
+const HeaderStyles = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid var(--black, black);
+  }
+`;
 
 const Header = () => (
-  <header>
+  <HeaderStyles>
     <div className="bar">
-      Sick Fits
-      <Link href="/">Home</Link>
+      <Logo>
+        <Link href="/">Home</Link>
+      </Logo>
     </div>
     <div className="sub-bar">
       <p>Search</p>
     </div>
-  </header>
+    <Nav />
+  </HeaderStyles>
 );
 export default Header;
